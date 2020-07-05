e100133b28c5974cfadf1ed5b2bcf089b7aa1059
Steven-Tang-rong <gzodc0045288@gmail.com>
2020-07-05 11:14:15 +0800

    Initial Commit

---
+++ LIFE MARK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LIFE MARK
+//
+//  Created by TANG,QI-RONG on 2020/7/5.
+//  Copyright © 2020 Steven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
